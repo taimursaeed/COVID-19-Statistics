453b2aed856bddf40ff65d6ec07b8b7fea4e35d0
Taimur <taimur.saeed.malik@gmail.com>
2020-03-25 13:58:50 +0500

    First

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var nurl;
+  var worldwide = false;
+  $("#country").on("change", function() {
+    worldwide = false;
+    $(this).val() == "ALL"
+      ? ((nurl = "https://thevirustracker.com/free-api?global=stats"),
+        (worldwide = true))
+      : (nurl =
+          "https://thevirustracker.com/free-api?countryTotal=" + $(this).val());
+
+    $.ajax({
+      url: nurl,
+      dataType: "json",
+      beforeSend: function() {
+        $(".count").hide();
+        $(".loader").show();
+      },
+      success: function(data) {
+        console.log(data);
+        $("#total div,#new div").each(function() {
+          var text;
+          worldwide
+            ? (text = data.results[0][$(this)[0].id])
+            : (text = data["countrydata"][0][$(this)[0].id]);
+          $(this)
+            .find(".count")
+            .text(text);
+        });
+        $(".loader").hide();
+        $(".count").show();
+      }
+    });
+  });
+});
