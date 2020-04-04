66b9b376de46f5ec050e0516e6c369097b7987c9
Taimur <taimur.saeed.malik@gmail.com>
2020-04-04 18:19:37 +0500

    -fix added proxy to bypass CORS policy

--- scripts/app.js
+++ scripts/app.js
@@ -2,59 +2,62 @@ var worldwide = false;
 var url = new URL(document.location);
 var params = url.searchParams;
 var countryID = params.get("country");
+var apiURL = "https://thevirustracker.com/free-api";
+var useProxy = true;
+
 countryID &&
 $("#country option[value=" + countryID.toUpperCase() + "]").length > 0
   ? (countryID = countryID.toUpperCase())
   : (countryID = "WW");
 
-$(document).ready(function() {
+$(document).ready(function () {
   $("#country").val(countryID);
   countryID == "WW"
-    ? ((worldwide = true),
-      showData("https://thevirustracker.com/free-api?global=stats"))
-    : showData(
-        "https://thevirustracker.com/free-api?countryTotal=" + countryID
-      );
+    ? ((worldwide = true), showData(apiURL + "?global=stats"))
+    : showData(apiURL + "?countryTotal=" + countryID);
 
-  $("#country").on("change", function() {
+  $("#country").on("change", function () {
     worldwide = false;
     var selectedValue = $(this).val();
     selectedValue == "WW"
-      ? ((worldwide = true),
-        showData("https://thevirustracker.com/free-api?global=stats"))
-      : showData(
-          "https://thevirustracker.com/free-api?countryTotal=" + selectedValue
-        );
+      ? ((worldwide = true), showData(apiURL + "?global=stats"))
+      : showData(apiURL + "?countryTotal=" + selectedValue);
     params.set("country", selectedValue);
     window.history.replaceState({}, "", `?country=${selectedValue}`);
   });
 });
 
 function showData(url) {
+  if (useProxy) {
+    $.ajaxPrefilter(function (options) {
+      if (options.crossDomain && jQuery.support.cors) {
+        options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
+      }
+    });
+  }
+
   $.ajax({
     url: url,
     dataType: "json",
-    beforeSend: function() {
+    beforeSend: function () {
       $(".loader").show();
       $(".count").hide();
       $(".loader").addClass("animate");
       $(".count").removeClass("animate");
     },
-    success: function(data) {
+    success: function (data) {
       // debugger;
-      $("#total>div,#new>div").each(function() {
+      $("#total>div,#new>div").each(function () {
         var text;
         worldwide
           ? (text = data.results[0][$(this)[0].id].toLocaleString())
           : (text = data["countrydata"][0][$(this)[0].id].toLocaleString());
-        $(this)
-          .find(".count")
-          .text(text);
+        $(this).find(".count").text(text);
       });
       $(".loader").hide();
       $(".count").show();
       $(".loader").removeClass("animate");
       $(".count").addClass("animate");
-    }
+    },
   });
 }
